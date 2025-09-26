5111574e5657e7e35b97ea82d9544b3524164549
dhavalbathe <dhavalbathe@gmail.com>
2025-09-26 09:38:35 +0530

    setup of smtp using nodemailer done

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 
 const {PORT} = require('./config/serverConfig');
+const {sendBasicEmail} = require('./services/email-service');
 
 const setUpAndStartServer = () => {
     const app = express();
@@ -10,6 +11,13 @@ const setUpAndStartServer = () => {
 
     app.listen(PORT, () => {
         console.log(`Server is running on PORT: ${PORT}`);
+
+        sendBasicEmail(
+            'tpassdhaval@gmail.com',
+            'dhavalbathe01@gmail.com',
+            'This is a test mail',
+            'This is the mail body, for testing purpose. Thank You !'
+        );
     });
 }
 
